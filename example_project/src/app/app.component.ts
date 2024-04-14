804566d6df428fe294816d5de9a189123f7f0283
Your Name <dinca.toni@gmail.com>
2024-04-14 17:08:52 +0200

    display block added

--- example_project/src/app/app.component.ts
+++ example_project/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextArea1Component } from '../components/atoms/text-area1/text-area1.component';
 import { SubmitButton1Component } from '../components/atoms/submit-button1/submit-button1.component';
+import { DisplayBlock1Component } from '../components/atoms/display-block1/display-block1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextArea1Component, SubmitButton1Component],
+  imports: [
+    RouterOutlet,
+    TextArea1Component,
+    SubmitButton1Component,
+    DisplayBlock1Component,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ example_project/src/components/atoms/display-block1/display-block1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'display-block1',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-block1.component.html',
+  styleUrl: './display-block1.component.css',
+})
+export class DisplayBlock1Component {}
