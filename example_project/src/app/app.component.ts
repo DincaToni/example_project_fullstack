41a433ede516305a0c4d54adda1d3ff355581b8e
Your Name <dinca.toni@gmail.com>
2024-04-14 16:49:46 +0200

    Intial submit button added

--- example_project/src/app/app.component.ts
+++ example_project/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextArea1Component } from '../components/atoms/text-area1/text-area1.component';
+import { SubmitButton1Component } from '../components/atoms/submit-button1/submit-button1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextArea1Component],
+  imports: [RouterOutlet, TextArea1Component, SubmitButton1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ example_project/src/components/atoms/submit-button1/submit-button1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'submit-button1',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit-button1.component.html',
+  styleUrl: './submit-button1.component.css',
+})
+export class SubmitButton1Component {}
