e68504ff63d22d5666501efa18e44cab2707def2
Your Name <dinca.toni@gmail.com>
2024-04-15 04:09:37 +0200

    Adding divs on click functionality added

--- example_project/src/app/app.component.ts
+++ example_project/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TextArea1Component } from '../components/atoms/text-area1/text-area1.component';
-import { SubmitButton1Component } from '../components/atoms/submit-button1/submit-button1.component';
-import { DisplayBlock1Component } from '../components/atoms/display-block1/display-block1.component';
+import { ExampleUIComponent } from '../components/templates/example-ui/example-ui.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    TextArea1Component,
-    SubmitButton1Component,
-    DisplayBlock1Component,
-  ],
+  imports: [RouterOutlet, ExampleUIComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- example_project/src/components/atoms/submit-button1/submit-button1.component.ts
+++ example_project/src/components/atoms/submit-button1/submit-button1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'submit-button1',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './submit-button1.component.html',
   styleUrl: './submit-button1.component.css',
 })
-export class SubmitButton1Component {}
+export class SubmitButton1Component {
+  @Input() results!: String[];
+  @Output() resultsChange = new EventEmitter<String[]>();
+
+  onSubmit = () => {
+    this.results.push('');
+    this.resultsChange.emit(this.results);
+    console.log(this.results);
+  };
+}
